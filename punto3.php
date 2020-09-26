fedc3bd87c019ca28e58bc0a2704c5050833d051
Sharkan262 <mr.lesc262@gmail.com>
2020-09-26 10:04:59 -0500

    Pequeña corrección

--- punto3.php
+++ punto3.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Punto 3</title>
-    <link rel="stylesheet" href="style/punto2.css">
+    <link rel="stylesheet" href="style/punto3.css">
     <link href="https://fonts.googleapis.com/css2?family=Concert+One&display=swap" rel="stylesheet">
     
 </head>
